8e021e03137f4fb04679d8bd4012eb6aa171c703
RisvanBCA2022 <mohammedrisvan16@gmail.com>
2024-05-03 16:31:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './index.scss'
+
+function App() {
+
+  return (
+    <>
+    <div className='App'>
+      Hello world
+    </div>
+    </>
+  )
+}
+
+export default App
