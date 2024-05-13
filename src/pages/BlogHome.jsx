778c362bfceb7b59d227ee17eabe178661a0a2fc
RisvanBCA2022 <mohammedrisvan16@gmail.com>
2024-05-13 17:07:13 +0530

    Blog Home layour created

--- src/pages/BlogHome.jsx
+++ src/pages/BlogHome.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
 
 const BlogHome = () => {
+
+    const blogs=[
+        {title:'Nodejs is a Runtime Enviornment',thumbnail:'https://kinsta.com/wp-content/uploads/2021/03/Nodejs-Architecture.png',body:'Nodejs is a Runtime enviornment not a framework',category:'Nodejs'},
+        {title:'Nodejs is a Runtime Enviornment',thumbnail:'https://kinsta.com/wp-content/uploads/2021/03/Nodejs-Architecture.png',body:'Nodejs is a Runtime enviornment not a framework',category:'Nodejs'},
+        {title:'Nodejs is a Runtime Enviornment',thumbnail:'https://kinsta.com/wp-content/uploads/2021/03/Nodejs-Architecture.png',body:'Nodejs is a Runtime enviornment not a framework',category:'Nodejs'},
+        {title:'Nodejs is a Runtime Enviornment',thumbnail:'https://kinsta.com/wp-content/uploads/2021/03/Nodejs-Architecture.png',body:'Nodejs is a Runtime enviornment not a framework',category:'Nodejs'}
+
+    ]
   return (
-    <div className="text-3xl font-bold underline">BlogHome</div>
+    <div>
+        <h1 className='md:text-4xl sm:text-2xl lg:text-6xl text-center p-14'>
+        <hr></hr>
+        <span className='m-10 font-semibold'>
+            SCRIBEWAY BLOGS
+        </span>
+            <hr></hr>
+        </h1>
+        <div className='flex'>
+            <div className='bg-red-400 h-20 w-1/2'> Blog1</div>
+            <div className='w-1/2'>
+            <div className='bg-orange-400 h-10'>Blog2</div>
+            <div className='bg-green-400 h-10'>Blog3</div>
+            </div>
+            
+        
+        </div>
+        <div className='w-full h-20 bg-orange-300'>
+        Blog4
+        </div>
+    </div>
   )
 }
 
